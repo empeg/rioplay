d66126930490a3a3ca42c13bb746861aacd62b1b
grgcombs <grgcombs>
2002-09-12 14:25:43 +0000

    Dynamically rearrange items for VFD size.

--- Screens/StatusScreen.cc
+++ Screens/StatusScreen.cc
@@ -31,12 +31,25 @@ StatusScreen::~StatusScreen(void) {
 
 
 void StatusScreen::Update(VFDLib &Display) {
-    int StringHeight = Display.getTextHeight(VFD_DEFAULT_FONT);
+    int vfd_width = VFDLib::vfd_width;
+    int vfd_height = VFDLib::vfd_height;
+    int vfd_default_font = VFDLib::vfd_default_font;
+    int StringHeight = Display.getTextHeight(vfd_default_font);
     int CurrentHeight = 0;
+    int vfd_font_time = VFD_FONT_TIME;
+    int timeX = 0;
+    int timeY = 0;
+    int randomX = 0;
+    int randomY = 0;
+    char randomText[] = "Random";
+
     pthread_mutex_lock(&ClassMutex);
+
+    if (Globals::hw_type == HWTYP_EMPEG) /* Empeg can't fit time font here */
+	    vfd_font_time = vfd_default_font;
     
     /* Set clipping area */
-    Display.setClipArea(0, 0, VFD_WIDTH, VFD_HEIGHT);
+    Display.setClipArea(0, 0, vfd_width, vfd_height);
     
     if(TitleArtistChanged == true) {
         Display.clear(VFDSHADE_BLACK);
@@ -49,37 +62,56 @@ void StatusScreen::Update(VFDLib &Display) {
      */
     #define BORDER_INFO 3
     CurrentHeight = BORDER_INFO;
-    Display.drawText(Title, 0, CurrentHeight, VFD_DEFAULT_FONT, -1);
+    Display.drawText(Title, 0, CurrentHeight, vfd_default_font, -1);
     
     CurrentHeight += BORDER_INFO + StringHeight;
-    Display.drawLineHorizClipped(CurrentHeight, 0, VFD_WIDTH, -1);
+    Display.drawLineHorizClipped(CurrentHeight, 0, vfd_width, VFDSHADE_DIM);
 
     CurrentHeight += BORDER_INFO + 1; /* '1' is the height of the horizontal line */
-    Display.drawText(Artist, 0, CurrentHeight, VFD_DEFAULT_FONT, -1);
+    Display.drawText(Artist, 0, CurrentHeight, vfd_default_font, -1);
 
     CurrentHeight += BORDER_INFO + StringHeight;
-    Display.drawText(Album, 0, CurrentHeight, VFD_DEFAULT_FONT, -1);
+    Display.drawText(Album, 0, CurrentHeight, vfd_default_font, -1);
         
     /* Display elapsed time */
     char TimeString[9];
     sprintf(TimeString, "%d:%02d", Minutes, Seconds);
 
-    /* Here we black out the area behind the track time */
-    Display.drawSolidRectangleClipped(VFD_WIDTH-2 - Display.getTextWidth(TimeString, VFD_FONT_TIME), 
-		VFD_HEIGHT-2 - Display.getTextHeight(VFD_FONT_TIME),
-            	VFD_WIDTH, VFD_HEIGHT, VFDSHADE_BLACK);
+    timeX = vfd_width - BORDER_INFO - 
+		Display.getTextWidth(TimeString, vfd_font_time);
 
-    Display.drawText(TimeString, VFD_WIDTH-2 - Display.getTextWidth(TimeString, VFD_FONT_TIME),
-            VFD_HEIGHT-2 - Display.getTextHeight(VFD_FONT_TIME), VFD_FONT_TIME, -1);
+    if (Globals::hw_type == HWTYP_EMPEG) /* Put time alongside album */ 
+	timeY = CurrentHeight;
+    else /* Put time at bottom right */
+	timeY = vfd_height - 2 - Display.getTextHeight(vfd_font_time);
     
+    /* Here we black out the area behind the track time */
+    Display.drawSolidRectangleClipped( timeX, timeY,
+            	vfd_width, vfd_height, VFDSHADE_BLACK);
+
+    Display.drawText(TimeString, timeX, timeY, vfd_font_time, -1);
+    
+    if (Globals::hw_type == HWTYP_EMPEG) 
+    { /* Put "Random" above time */
+    	randomX = vfd_width - BORDER_INFO - 
+	   Display.getTextWidth(randomText, VFD_FONT_SMALL);
+    	randomY = timeY - BORDER_INFO - Display.getTextHeight(VFD_FONT_SMALL);
+    }
+    else
+    { /* Put "Random" at bottom right of screen */
+	randomX = 1;
+	randomY = vfd_height - 2 - Display.getTextHeight(VFD_FONT_SMALL);
+    }
+
     /* Show random status */
     if(Globals::Playlist.GetRandom() == true) {
-        Display.drawText("RANDOM", 1, VFD_HEIGHT-2 - Display.getTextHeight(VFD_FONT_SMALL), 
-		VFD_FONT_SMALL, -1);
+        Display.drawText(randomText, randomX, randomY, VFD_FONT_SMALL, -1);
     }
     else {
-        Display.drawSolidRectangleClipped(0, VFD_HEIGHT-2 - Display.getTextHeight(VFD_FONT_SMALL) - 1,
-                Display.getTextWidth("RANDOM", VFD_FONT_SMALL) + 2, VFD_HEIGHT, VFDSHADE_BLACK);
+        Display.drawSolidRectangleClipped(randomX, randomY,
+                randomX + Display.getTextWidth(randomText, VFD_FONT_SMALL), 
+		randomY + Display.getTextHeight(VFD_FONT_SMALL),
+	       	VFDSHADE_BLACK);
     }
     
     pthread_mutex_unlock(&ClassMutex);
